b16b811a61ddc23e5520f4636bb290df2aac16f4
Boyan Stoilov <boyan_stoilov@werum.net>
2021-07-22 18:56:54 +0300

    added update/delete of prod usage

--- src/main/java/com/example/recipes/controller/ProductUsageController.java
+++ src/main/java/com/example/recipes/controller/ProductUsageController.java
@@ -2,16 +2,19 @@ package com.example.recipes.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.recipes.model.ProductUsage;
@@ -29,7 +32,6 @@ public class ProductUsageController {
 	public ResponseEntity<List<ProductUsage>> getAllProductUsage () {
 		try {
 			List<ProductUsage> prodUsage = new ArrayList<>();
-			
 			
 			prodUsageRepository.findAll().forEach(prodUsage::add);
 			
@@ -49,14 +51,38 @@ public class ProductUsageController {
 		try {
 			ProductUsage _prodUsage = prodUsageRepository
 					.save(new ProductUsage(prodUsage.getProduct(),
-											prodUsage.getProductQuantity(),
-											prodUsage.getProductUOM(),
-											prodUsage.getRecipeId()));
+										   prodUsage.getProductQuantity(),
+										   prodUsage.getProductUOM(),
+										   prodUsage.getRecipeId()));
+			
 			return new ResponseEntity<>(_prodUsage,  HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
+	@PutMapping("/product-usage/{id}")
+	public ResponseEntity<ProductUsage> updateProductUsage (@PathVariable("id") long id, @RequestBody ProductUsage prodUsage) {
+			Optional<ProductUsage> productUsageData = prodUsageRepository.findById(id);
+			
+			if (productUsageData.isPresent()) {
+				ProductUsage _prodUsage = new ProductUsage(prodUsage.getProduct(),
+						   prodUsage.getProductQuantity(),
+						   prodUsage.getProductUOM(),
+						   prodUsage.getRecipeId()))
+			}
+		
+	}
+
+	@DeleteMapping("/product-usage/{id}")
+	public ResponseEntity<ProductUsage> deleteProductUsage (@PathVariable("id") long id) {
+		try {
+			prodUsageRepository.deleteById(id);
+			
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 }
